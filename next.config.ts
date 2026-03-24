2b69ec58b756e7893a2b2d02a9def562366f5bd8
Your Correct GitHub Username <alieelpozo3@gmail.com>
2026-03-24 07:02:22 -0400

    chore: deploy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  output: "export",
+  basePath: "/Class_487_demo",
 };
 
 export default nextConfig;
